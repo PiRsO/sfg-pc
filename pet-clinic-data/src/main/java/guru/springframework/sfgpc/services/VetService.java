d3ac80a94b51a49c791704c313a0053b529b6651
Denes Katona <denes.katona@attrecto.com>
2018-09-25 10:44:51 +0200

    add service layer

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpc/services/VetService.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpc.services;
+
+import guru.springframework.sfgpc.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet owner);
+
+    Set<Vet> findAll();
+}
