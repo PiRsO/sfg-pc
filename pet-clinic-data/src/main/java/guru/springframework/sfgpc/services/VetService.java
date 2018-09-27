4b0d73e6465ced3eed651b942399507b9525bdb6
Denes Katona <denes.katona@attrecto.com>
2018-09-27 08:19:52 +0200

    Refactor service layer.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpc/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpc/services/VetService.java
@@ -2,13 +2,7 @@ package guru.springframework.sfgpc.services;
 
 import guru.springframework.sfgpc.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
