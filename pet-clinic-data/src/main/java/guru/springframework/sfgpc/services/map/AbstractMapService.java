8d2375d9c1598607271bb67f6321ecbbdde71f6c
Denes Katona <denes.katona@attrecto.com>
2018-10-08 16:02:22 +0200

    auto generate IDs

--- pet-clinic-data/src/main/java/guru/springframework/sfgpc/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpc/services/map/AbstractMapService.java
@@ -1,13 +1,16 @@
 package guru.springframework.sfgpc.services.map;
 
+import guru.springframework.sfgpc.model.BaseEntity;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class AbstractMapService<T, ID> {
+public class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
        return new HashSet<>(map.values());
@@ -17,9 +20,14 @@ public class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
+    T save(T object) {
 
-        map.putIfAbsent(id, object);
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }
 
         return object;
     }
@@ -32,5 +40,9 @@ public class AbstractMapService<T, ID> {
         map.entrySet()
                 .removeIf(entry -> entry.getValue()
                         .equals(object));
+    }
+
+    private Long getNextId() {
+        return map.isEmpty() ? 1L : Collections.max(map.keySet()) + 1;
     }
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpc/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpc/controllers/VetController.java
@@ -1,13 +1,25 @@
 package guru.springframework.sfgpc.controllers;
 
+import guru.springframework.sfgpc.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping(value = {"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    private final VetService vetService;
+
+    public VetController(final VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping(value = {"", "/index", "/index.html"})
+    public String listVets(final Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 
