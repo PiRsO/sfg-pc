b5b4154f11b5a4076e186e914aba7e8c8bb93690
Denes Katona <denes.katona@attrecto.com>
2018-10-10 10:11:49 +0200

    add Pets to initial data

--- pet-clinic-data/src/main/java/guru/springframework/sfgpc/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpc/services/map/OwnerServiceMap.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgpc.services.map;
 
 import guru.springframework.sfgpc.model.Owner;
+import guru.springframework.sfgpc.model.Pet;
 import guru.springframework.sfgpc.services.OwnerService;
+import guru.springframework.sfgpc.services.PetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(final PetTypeService petTypeService, final PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -21,7 +31,24 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        if (object != null) {
+            object.getPets().forEach(pet -> {
+                if (pet.getPetType() != null) {
+                    if (pet.getPetType().getId() == null) {
+                        pet.setPetType(petTypeService.save(pet.getPetType()));
+                    }
+                } else {
+                    throw new RuntimeException("Pet Type is required");
+                }
+                if (pet.getId() == null) {
+                    Pet savedPet = petService.save(pet);
+                    pet.setId(savedPet.getId());
+                }
+            });
+            return super.save(object);
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/guru/springframework/sfgpc/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpc/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpc.bootstrap;
 
 import guru.springframework.sfgpc.model.Owner;
+import guru.springframework.sfgpc.model.Pet;
 import guru.springframework.sfgpc.model.PetType;
 import guru.springframework.sfgpc.model.Vet;
 import guru.springframework.sfgpc.services.OwnerService;
@@ -8,6 +9,8 @@ import guru.springframework.sfgpc.services.VetService;
 import guru.springframework.sfgpc.services.map.PetTypeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -41,12 +44,31 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("5555551115");
+
+        Pet mikePet = new Pet();
+        mikePet.setPetType(savedDogPetType);
+        mikePet.setOwner(owner1);
+        mikePet.setBirthDate(LocalDate.now());
+        mikePet.setName("Rosco");
+        owner1.getPets().add(mikePet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("5555551115");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setOwner(owner2);
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
