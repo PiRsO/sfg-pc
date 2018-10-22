cccde4a54701984a455d27f48a3cf98cc9420153
Denes Katona <denes.katona@attrecto.com>
2018-10-22 09:15:18 +0200

    Annotate object with JPA annotation

--- pet-clinic-data/src/main/java/guru/springframework/sfgpc/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpc/model/Pet.java
@@ -1,10 +1,21 @@
 package guru.springframework.sfgpc.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @ManyToOne
+    @JoinTable(name = "pet_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
     private LocalDate birthDate;
     private String name;
