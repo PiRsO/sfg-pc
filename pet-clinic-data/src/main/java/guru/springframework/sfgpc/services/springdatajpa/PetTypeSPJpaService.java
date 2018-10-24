78cb8ec0fc19d3aec034c2cf8cd1631203a1b1bc
Denes Katona <denes.katona@attrecto.com>
2018-10-24 19:28:48 +0200

    implement spring data jps PetType service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpc/services/springdatajpa/PetTypeSPJpaService.java
@@ -0,0 +1,48 @@
+package guru.springframework.sfgpc.services.springdatajpa;
+
+import guru.springframework.sfgpc.model.PetType;
+import guru.springframework.sfgpc.repositories.PetTypeRepository;
+import guru.springframework.sfgpc.services.map.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSPJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSPJpaService(final PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(final Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(final PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(final PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deletedById(final Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
